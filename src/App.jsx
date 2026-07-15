f0f35c0ce12835cebb515263889f8cfc53558cc2
AgnanKen <agnan.jaisil@gmail.com>
2026-07-15 10:53:29 +0530

    Add & View Driver Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddDriver from './components/AddDriver'
+import ViewDriver from './components/ViewDriver'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddDriver/>
+      <ViewDriver/>
     </>
   )
 }
