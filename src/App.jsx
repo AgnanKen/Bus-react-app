86740265e98d87ed0013692bb4d870b22eae6fdf
AgnanKen <agnan.jaisil@gmail.com>
2026-07-15 11:11:56 +0530

    Nav complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddDriver from './components/AddDriver'
 import ViewDriver from './components/ViewDriver'
+import AddBus from './components/AddBus'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddDriver/>
-      <ViewDriver/>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/addbus' element={<AddBus/>}/>
+      <Route path='/adddriver' element={<AddDriver/>}/>
+      <Route path='/viewdriver' element={<ViewDriver/>}/>
+      {/* <Route path='/' element={<Viewb/>}/> */}
+
+      </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
--- src/components/AddBus.jsx
+++ src/components/AddBus.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Navbar from "./Navbar";
 
 
 const AddBus = () => {
@@ -41,8 +42,7 @@ const AddBus = () => {
 
     return (
         <div>
-
-
+            <Navbar/>
             <div className="container mt-4">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
